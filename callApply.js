b7672b735bbe327758499086bce78cfe8cd51cc9
Kazi Salith Ur Rahman <salith.rahman107@gmail.com>
2023-12-06 23:38:05 -0330

    bind in obeject to object

--- callApply.js
+++ callApply.js
@@ -10,6 +10,26 @@ const specialPerson = {
   },
 };
 
-specialPerson.chargeBill(1000);
-specialPerson.chargeBill(100);
-console.log(specialPerson.salary);
+const friendlyPerson = {
+  fisrtName: "Pathok",
+  LastName: "shibBujhe",
+  salary: 30000,
+};
+
+const greatPerson = {
+  fisrtName: "Fiha",
+  LastName: "Shomikoron",
+  salary: 50000,
+};
+
+const friendlyChargeBill = specialPerson.chargeBill.bind(friendlyPerson);
+
+friendlyChargeBill(6000);
+
+// specialPerson.chargeBill(1000);
+// specialPerson.chargeBill(100);
+// console.log(specialPerson.salary);
+console.log(friendlyPerson);
+friendlyChargeBill(2000);
+console.log(friendlyPerson);
+console.log(specialPerson);
